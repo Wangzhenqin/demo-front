5798d7e81e62de70209c6515c21ed212289b6c7c
Wangzhenqin <714004973@qq.com>
2020-05-06 18:08:03 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container style="height: auto; border: 1px solid #eee">
+      <el-header style="text-align: right; font-size: 12px">
+        <span style="float :left; font-size:20px">毕业设计管理选题系统</span>
+        <el-dropdown v-if="$store.state.isEnter">
+          <i class="el-icon-setting" style="margin-right: 15px"></i>
+          <el-dropdown-menu slot="dropdown">
+            <div @click="exit">
+              <el-dropdown-item>退出</el-dropdown-item>
+            </div>
+          </el-dropdown-menu>
+        </el-dropdown>
+        <span>{{$store.state.user.name}}</span>
+      </el-header>
+      <el-container style="height: 740px; border: 1px solid #eee">
+        <tab-bar></tab-bar>
+        <router-view ></router-view>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TabBar from "./components/tabbar/tab";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TabBar
+  },
+  data() {
+    return {
+      success: null,
+    };
+  },
+  methods: {
+    exit() {
+      Object.assign(this.$data, this.$options.data());
+      this.$store.commit('exit');
+      this.$router.replace("/enter");
+    }
+  },
+  created() {
+    this.$store.commit('exit');
+    this.$router.replace("/enter");
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "assets/css/base.css";
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
 }
 </style>
